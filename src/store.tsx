57e00a64528e95240386fe3648e17ee10421b4bd
Steven Frady <sfrady20@gmail.com>
2024-05-02 00:44:45 -0400

    make store

--- src/store.tsx
+++ src/store.tsx
@@ -1,21 +1,11 @@
 "use client";
 
-import { ReactNode, createContext, useContext, useMemo } from "react";
-import { create } from "zustand";
-import { immer } from "zustand/middleware/immer";
+import { makeStore } from "./utils/make-store";
 
-declare interface AppState {}
-
-const makeStore = () => create(immer<AppState>((set, get) => ({})));
-
-const StoreContext = createContext<AppState>({});
-
-export function StoreProvider(props: { children: ReactNode }) {
-  const { children } = props;
-  const store = useMemo(makeStore, []);
-  return (
-    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
-  );
-}
-
-export const useStore = () => useContext(StoreContext);
+export const { Provider: StoreProvider, use: useStore } = makeStore(
+  (set, get) => {
+    return {
+      test: true,
+    };
+  }
+);
--- src/vars.ts
+++ src/vars.ts
@@ -1,5 +1,6 @@
 export const SECRET = process.env.SECRET || "njifrbf0ybrf10ugfr93tvg13";
 
 export const DB_URL = (process.env.DB_URL || process.env.DATABASE_URL)!;
+
 export const DB_TABLE_PREFIX =
   process.env.DB_TABLE_PREFIX || process.env.DATABASE_TABLE_PREFIX;
