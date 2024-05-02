06f5a49a40aea1672a73a3b0f7aa86356d4b42e8
Steven Frady <sfrady20@gmail.com>
2024-05-02 01:00:32 -0400

    nest home page

--- src/services/auth.tsx
+++ src/services/auth.tsx
@@ -2,10 +2,25 @@ import NextAuth from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { SECRET } from "@/vars";
 import { db } from "@/services/db";
+import { Provider } from "@auth/core/providers";
+
+const providers: Provider[] = [];
+
+export const providerMap = providers.map((provider) => {
+  if (typeof provider === "function") {
+    const providerData = provider();
+    return { id: providerData.id, name: providerData.name };
+  } else {
+    return { id: provider.id, name: provider.name };
+  }
+});
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: DrizzleAdapter(db),
-  providers: [],
   secret: SECRET,
+  adapter: DrizzleAdapter(db),
+  providers,
   trustHost: true,
+  pages: {
+    signIn: "/signin",
+  },
 });
