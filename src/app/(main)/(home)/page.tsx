81fceccbf557999b9fb7d2e30127dd2124849de1
sFrady20 <sfrady20@gmail.com>
2025-01-27 16:33:01 -0500

    redo

--- src/app/(main)/(home)/page.tsx
+++
@@ -1,7 +0,0 @@
-export default async function () {
-  return (
-    <main className="flex-1 flex flex-col items-center justify-center">
-      <div>Welcome</div>
-    </main>
-  );
-}
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "earthling-ui/button";
+
+export default async function () {
+  return (
+    <div className="flex-1 flex items-center justify-center min-h-screen">
+      <Button variant={"outline"}>Hello Earthling</Button>
+    </div>
+  );
+}
