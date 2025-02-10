f517f95031d84ec287e75beda9225c49ee6e66f8
Steven Frady <sfrady20@gmail.com>
2025-02-10 13:48:22 -0500

    update earthling prop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "earthling-ui/button";
 export default async function () {
   return (
     <div className="flex-1 flex items-center justify-center min-h-screen">
-      <Button variant={"outline"}>Hello Earthling</Button>
+      <Button material={"outline"}>Hello Earthling</Button>
     </div>
   );
 }
